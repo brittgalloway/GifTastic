7cfbc87f69e60c3899a64814e6de1c1001678a57
BrittGall <crlnfllr@gmail.com>
2019-08-29 00:56:08 -0500

    psuedo code and beginning code

--- assets/javascript/gifapp.js
+++ assets/javascript/gifapp.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
+  //array of movie names
   const topicArr = [
     "spirted away",
     "princess mononoke",
     "Nauscaa",
     "the cat returns"
   ];
+  const title;
+  for (let index = 0; index < topicArr.length; index++) {
+     title = topicArr[index];
+  }
+  //link to giphy and write query(limit 10)
+  const queryURL =
+    "http://api.giphy.com/v1/gifs/search?q=" +
+    title +
+    "&api_key=4xTnjBJYg6aueD0kdEHtsoUcQ450SVGu&limit=10";
+
+    //ajax
+    
+  //make buttons from the array
+
+  //
 });
