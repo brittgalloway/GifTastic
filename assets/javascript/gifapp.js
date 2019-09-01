01ae000e601a15693ad7544adddce331c6cfc8d3
BrittGall <crlnfllr@gmail.com>
2019-08-31 20:37:42 -0500

    edited main height, img width

--- assets/javascript/gifapp.js
+++ assets/javascript/gifapp.js
@@ -3,21 +3,24 @@ $(document).ready(function() {
   const topicArr = [
     "Spirted Away",
     "Princess Mononoke",
-    "Nauscaa",
+    "Nausicaa",
     "the Cat Returns"
   ];
-  let title;
+
+  let title = "";
   for (let index = 0; index < topicArr.length; index++) {
     title = topicArr[index];
     //creates buttons from items in the array
     $("#buttons").append(
-      "<button data-search=" + title + ">" + title + "</button>"
+      "<button data-type=" + title + ">" + title + "</button>"
     );
+
     console.log($("button"));
   }
   //on click function
 
   $("#buttons").on("click", function() {
+    var person = $(this).attr("data-type");
     //link to giphy and write query(limit 10)
     const queryURL =
       "http://api.giphy.com/v1/gifs/search?q=" +
@@ -25,9 +28,30 @@ $(document).ready(function() {
       "&api_key=4xTnjBJYg6aueD0kdEHtsoUcQ450SVGu&limit=10";
     console.log(title);
     //ajax to get gifs from giphy
-    $.ajax({ url: queryURL, method: "GET" }).done(function(gifs) {
-      console.log(gifs);
-    });
+    $.ajax({ url: queryURL, method: "GET" })
+      .done(function(gifs) {
+        console.log(gifs);
+      })
+      .then(function(response) {
+        const results = response.data;
+        for (let ratings = 0; ratings < results.length; ratings++) {
+          if (
+            results[ratings].rating !== "r" &&
+            results[ratings].rating !== "pg-13"
+          ) {
+            let rating = results[ratings].rating;
+            let gifWrap = $("<div>");
+            let p = $("<p>").text("Rating: " + rating);
+            let movieGif = $(
+              "<img src=" + results[ratings].images.fixed_height.url + ">"
+            );
+            //adds p amd movieGif to the gifWrap div
+            gifWrap.append(p);
+            gifWrap.append(movieGif);
+            //adds gifwrap to the page
+            $("#images").append(gifWrap);
+          }
+        }
+      });
   });
-  //
 });
